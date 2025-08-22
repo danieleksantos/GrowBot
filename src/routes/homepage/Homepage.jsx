500e474d3f08517eb69ed872a249717bc426280f
Daniele Karina dos Santos <danimistron@gmail.com>
2025-08-22 08:46:25 -0300

    style: adjustment responsive mobile

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -4,28 +4,31 @@ import './homepage.css'
 
 const Homepage = () => {
     return (
-        <div className='homepage'>
-            <img src="/orbital.png" alt="orbital image" className='orbital'/>
-            <div className="left">
-                <h1>GrowBot</h1>
-                <h2>Esclareça todas as suas dúvidas sobre o mercado tech</h2>
-                <p>Por onde começo estudar? Quais são as fontes mais confiáveis? Onde encontro vagas? Como me preparar para entrevistas?...</p>
-                <Link to="/chat" className='link-action'>Confira tudo AQUI</Link>
-            </div>
-
-            <div className="right">
-                <div className="imgContainer">
-                    <div className="bgContainer">
-                        <div className="bg"></div>
-                    </div>
-                    <img src="/bot.png" alt="Growbot"  className='bot'/>
+        <>
+            <div className='homepage'>
+                <img src="/orbital.png" alt="orbital image" className='orbital'/>
+                <div className="left">
+                    <h1>GrowBot</h1>
+                    <h2>Esclareça todas as suas dúvidas sobre o mercado tech</h2>
+                    <p>Por onde começo estudar? Quais são as fontes mais confiáveis? Onde encontro vagas? Como me preparar para entrevistas?...</p>
+                    <Link to="/chat" className='link-action'>Confira tudo AQUI</Link>
                 </div>
+
+                <div className="right">
+                    <div className="imgContainer">
+                        <div className="bgContainer">
+                            <div className="bg"></div>
+                        </div>
+                        <img src="/bot.png" alt="Growbot"  className='bot'/>
+                    </div>
+                </div>
+                
             </div>
             <div className="terms">
                 <img src="/logo-blue.png" alt="logo GrowBot" />
                 <p>Desenvolvido por Daniele Karina dos Santos</p>
             </div>
-        </div>
+        </>
         
     )
 }
